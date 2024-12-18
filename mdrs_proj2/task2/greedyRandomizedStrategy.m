7d90c43f6b4df56005ca86456cd5e9f06a4b7069
João Gaspar <j.gaspar@ua.pt>
2024-12-18 02:48:14 +0000

    cansei

--- mdrs_proj2/task2/greedyRandomizedStrategy.m
+++ mdrs_proj2/task2/greedyRandomizedStrategy.m
@@ -1,23 +1,30 @@
-function [sol, load, Loads] = greedyRandomizedStrategy(nNodes, Links, T, sP, nSP)
-    nFlows = size(T, 1);
+function [sol, load] = greedyRandomizedStrategy(nNodes, Links, T, sP, nSP)
+    nFlows = size(T,1);
+    % random order of flows 
+    randFlows = randperm(nFlows);
     sol = zeros(1, nFlows);
-    bestLoad = inf;
-    bestLoads = [];
 
-    for f = 1:nFlows
-        bestPathLoad = inf;
-        for p = 1:nSP(f)
-            sol(f) = p;
+    % iterate through each flow
+    for flow = randFlows
+        path_index = 0;
+        best_load = inf;
+
+        % test every path "possible" in a certain load
+        for path = 1 : nSP(flow)
+            % try the path for that flow
+            sol(flow) = path;
+            % calculate loads
             Loads = calculateLinkLoads(nNodes, Links, T, sP, sol);
             load = max(max(Loads(:, 3:4)));
-            if load < bestPathLoad
-                bestPathLoad = load;
-                bestPath = p;
+            
+            % check if the current load is better then bestLoad
+            if load < best_load
+                % change index of path and load
+                path_index = path;
+                best_load = load;
             end
         end
-        sol(f) = bestPath;
+        sol(flow) = path_index;
     end
-
-    Loads = calculateLinkLoads(nNodes, Links, T, sP, sol);
-    load = max(max(Loads(:, 3:4)));
+    load = best_load;
 end
